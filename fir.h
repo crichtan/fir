48ac57e7361315bd3bdc4458b99c99e3984688e4
uamana <sjuriev@gmail.com>
2014-12-16 14:30:14 +0300

    Create fir.h

---
+++ fir.h
@@ -0,0 +1,20 @@
+/* FIR filter */
+
+#ifndef FIR_H
+#define FIR_H
+
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct FIR_filter {
+    int length;
+    int count;
+    double *h;
+    double *delay_line;
+} FIR_filter;
+
+FIR_filter* FIR_init(int length, double *h);
+double FIR_get_sample(FIR_filter *filter, double input);
+void FIR_destroy(FIR_filter *filter);
+
+#endif // FIR_H
